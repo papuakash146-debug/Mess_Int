8097bf5c03542dcbdad158c1662ae86a279ecafe
Akash Dev <akashddev200@gmail.com>
2026-02-18 00:27:40 +0530

    backend updated

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const usageRoutes = require('./routes/usageRoutes');
 
 const app = express();
 app.use(express.json());
-
+const cors = require('cors');
+app.use(cors());
 
 mongoose.connect("mongodb+srv://mess_user:mess123@cluster0.pvegjbp.mongodb.net/mess_Int")
   .then(() => console.log("MongoDB Connected Successfully"))
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemController');
 
+router.get('/', async (req, res) => {
+  try {
+    const items = await Item.find().sort({ name: 1 }); // optional: sort alphabetically
+    res.json(items);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
 router.post('/', itemController.addItem);
 router.get('/summary', itemController.getStockSummary);
 router.get('/report', itemController.getThirtyDaysReport);
